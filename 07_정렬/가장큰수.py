e4b69c9e505f72bdb29fb0f8f1daf4600f302af3
leeyejin1231 <82494506+leeyejin1231@users.noreply.github.com>
2023-08-24 17:40:55 +0900

    0824

--- 07_정렬/가장큰수.py
+++ 07_정렬/가장큰수.py
@@ -1,6 +1,15 @@
-from functools import cmp_to_key
-
-def solution(numbers):
-    numbers = list(map(str, numbers))
-    numbers.sort(key=cmp_to_key(lambda x, y: int(x+y) - int(y+x)), reverse=True)
+def solution(numbers):    
+    numbers = list(map(str, numbers))             
+    numbers.sort(key = lambda x : x*3, reverse=True)  
+        
     return str(int(''.join(numbers)))
+
+
+
+3
+30
+301
+
+333
+303030
+3013013031
--- 08_이진 탐색/정렬된배열에서특정.py
+++ 08_이진 탐색/정렬된배열에서특정.py
@@ -10,22 +10,22 @@
 def start_num(array, target, start, end):
     while start <= end:
         mid = (start + end) // 2
-        if array[mid] < target:
+        if mid == 0 or (array[mid] == target and array[mid-1] != target):
+            return mid
+        elif array[mid] < target:
             start = mid + 1
         else:
-            if mid == 0 or (array[mid] == target and array[mid-1] != target):
-                return mid
             end = mid - 1
     return None
 
 def end_num(array, target, start, end):
     while start <= end:
         mid = (start + end) // 2
-        if array[mid] > target:
+        if mid == len(array)-1 or (array[mid] == target and array[mid+1] != target):
+            return mid
+        elif array[mid] > target:
             end = mid - 1
         else:
-            if mid == len(array)-1 or (array[mid] == target and array[mid+1] != target):
-                return mid
             start = mid + 1
     return None
 
@@ -35,7 +35,7 @@ array = list(map(int, input().split()))
 start_index = start_num(array, x, 0, n-1)
 end_index = end_num(array, x, 0, n-1)
 
-if start_index == None and end_index == None:
+if start_index == None or end_index == None:
     print(-1)
 else:
     print(end_index - start_index + 1)
